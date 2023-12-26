bc77915df710854351edc74cb5889be7bdd03b79
JackieChheng <jackiechheng4@gmail.com>
2023-12-26 15:26:07 -0700

    updated tag-routes.js file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
 });
 
 // get one product
