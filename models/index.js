cc634f80c3d8787aa235757ba367a3e7533c66d2
JackieChheng <jackiechheng4@gmail.com>
2024-02-05 13:04:39 -0700

    updated models/index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsto(Category, {
+  foreign_key: 'category:id',
+})
 
 // Categories have many Products
+Category.belongstoMany(Product, {
+  through: {
+    model: ProductTag,
+    foreign_key: 'product_id',
+  },
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongstoMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreign_key: 'product_id',
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongstoMany(Product, {
+  through: {
+    model: ProductTag,
+    foreign_key: 'product_id',
+  },
+});
 
 module.exports = {
   Product,
