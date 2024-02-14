3b252ca1620109aea66e9df0051b36368794816c
JackieChheng <jackiechheng4@gmail.com>
2024-02-13 17:16:03 -0700

    updated routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,19 +9,24 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
       include: [
-        {
-          model: Category,
-          attributes: ['category_name']
-        },
-        {
-          model: Tag,
-          attributes: ['tag_name'],
-          through: ProductTag,
-          as: 'tags'
-        }
-      ]
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ],
     });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No products found!' });
+      return;
+    }
+
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -29,33 +34,33 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', async(req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findByPk(req.params.id, {
+    const productData = await Product.findByPk(req.params.id,{
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
       include: [
-        {
-          model: Category,
-          attributes: ['category_name']
-        },
-        {
-          model: Tag,
-          attributes: ['tag_name'],
-          through: ProductTag,
-          as: 'tags'
-        }
-      ]
-    })
-  
-  if (!productData) {
-    res.status(404).json({message: 'No product found with this id!'});
-    return;
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+      model: Tag,
+      attributes: ['tag_name']
+      }
+    ],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No products found!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
   }
-  res.status(200).json(productData);
-} catch (err) {
-  res.status(500).json(err);
-}
 });
 
 // create new product
@@ -132,20 +137,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', async(req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-  try{
+  try {
     const productData = await Product.destroy({
       where: {
-        id: req.params.id
-      }
-    })
+        id: req.params.id,
+      },
+    });
+
     if (!productData) {
-      res.status(404).json({message: 'No product found with this id!'});
+      res.status(404).json({ message: 'No product found with that id!' });
       return;
     }
+
     res.status(200).json(productData);
-  }catch (err){
+  } catch (err) {
     res.status(500).json(err);
   }
 });
